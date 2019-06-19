81a1bd3f631422b66b7cd5181ab289522dabb438
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-06-19 08:27:44 -0300

    alterando dados do db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyA-ModLRq6hOMhrAuAy3NvHh-Eu_K5E3n4",
-    authDomain: "projeto-teste-f044d.firebaseapp.com",
-    databaseURL: "https://projeto-teste-f044d.firebaseio.com",
-    projectId: "projeto-teste-f044d",
-    storageBucket: "projeto-teste-f044d.appspot.com",
-    messagingSenderId: "612638745439",
-    appId: "1:612638745439:web:264acb9c82b8e7ef"
-  }
+  
 };
